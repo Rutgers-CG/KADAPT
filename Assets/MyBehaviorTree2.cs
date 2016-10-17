a56851acc2dc7afb24a5bf3a728a95bc98632574
mahyarkoy <m.khayatkhoei@gmail.com>
2016-10-17 16:38:51 -0400

    added textures, and scene 2.

--- Assets/MyBehaviorTree2.cs
+++ Assets/MyBehaviorTree2.cs
@@ -3,12 +3,13 @@ using System;
 using System.Collections;
 using TreeSharpPlus;
 
-public class MyBehaviorTree : MonoBehaviour
+public class MyBehaviorTree2 : MonoBehaviour
 {
 	public Transform wander1;
 	public Transform wander2;
 	public Transform wander3;
 	public GameObject participant;
+	public GameObject police;
 
 	private BehaviorAgent behaviorAgent;
 	// Use this for initialization
@@ -18,11 +19,11 @@ public class MyBehaviorTree : MonoBehaviour
 		BehaviorManager.Instance.Register (behaviorAgent);
 		behaviorAgent.StartBehavior ();
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
 	{
-	
+
 	}
 
 	protected Node ST_ApproachAndWait(Transform target)
@@ -33,11 +34,15 @@ public class MyBehaviorTree : MonoBehaviour
 
 	protected Node BuildTreeRoot()
 	{
+		Val<float> pp = Val.V (() => police.transform.position.z);
+		Func<bool> act = () => (police.transform.position.z > 10);
 		Node roaming = new DecoratorLoop (
-						new SequenceShuffle(
-						this.ST_ApproachAndWait(this.wander1),
-						this.ST_ApproachAndWait(this.wander2),
-						this.ST_ApproachAndWait(this.wander3)));
-		return roaming;
+			new Sequence(
+				this.ST_ApproachAndWait(this.wander1),
+				this.ST_ApproachAndWait(this.wander2),
+				this.ST_ApproachAndWait(this.wander3)));
+		Node trigger = new DecoratorLoop (new LeafAssert (act));
+		Node root = new DecoratorLoop (new DecoratorForceStatus (RunStatus.Success, new SequenceParallel(trigger, roaming)));
+		return root;
 	}
 }
